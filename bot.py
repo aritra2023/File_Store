d75da6d7ef85d900b377514ea69358dd5915aeaf
Aritra Mahatma <128917736+aritra2023@users.noreply.github.com>
2025-08-10 16:51:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -118,7 +118,7 @@ class Bot(Client):
         """Run the bot."""
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.start())
-        self.LOGGER(__name__).info("Bot is now running. Thanks to @rohit_1888")
+        self.LOGGER(__name__).info("Bot is now running. Thanks to @ownnnner")
         try:
             loop.run_forever()
         except KeyboardInterrupt:
